b657af51d79ba5463a0c96d074f5199797be247a
yhotta240 <yhotta240@icloud.com>
2025-07-05 11:59:58 +0900

    feat: enhance Card component with Badge support and improve layout styling

--- src/components/layout/cards.tsx
+++ src/components/layout/cards.tsx
@@ -13,10 +13,12 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
+import { Badge } from "../ui/badge";
 
 export default function cards({ items }: any) {
   const [chromeFilter, setChromeFilter] = useState(false);
   const [vsCodeFilter, setVsCodeFilter] = useState(false);
+  // console.log(items);
   return (
     <div>
       <h1 className="font-bold text-xl my-3">拡張機能</h1>
@@ -40,7 +42,7 @@ export default function cards({ items }: any) {
         </Button>
       </div>
 
-      <div className="grid grid-cols-2 md:grid-cols-3 gap-4">
+      <div className="grid grid-cols-2 md:grid-cols-3 gap-3">
         {items
           .sort((a: any, b: any) => {
             if (a[0] < b[0]) {
@@ -51,21 +53,47 @@ export default function cards({ items }: any) {
           .map((item: any, index: number) => (
             <Card
               key={index}
-              className="transition-transform duration-300 hover:scale-102 hover:shadow-md hover:shadow-gray-500 hover:cursor-pointer"
+              className="rounded-md gap-4 transition-transform duration-300 hover:scale-102 hover:shadow-md hover:shadow-gray-500 hover:cursor-pointer"
             >
-              <img src={item[8]} alt="" />
-              <CardHeader>
+              <div className="relative">
+                {item[4] !== "—" ? (
+                  <div className="bg-gray-900 text-yellow-400 rounded-full opacity-80 w-fit absolute top-2 px-2 right-2 ">
+                    {item[4]}
+                  </div>
+                ) : null}
+                <img src={item[8]} alt="" />
+              </div>
+              <CardHeader className="px-4">
                 <CardTitle>
                   <a
                     href={item[7]}
-                    className="hover:underline"
+                    className="hover:underline line-clamp-2"
                     rel="noopener noreferrer"
                     target="_blank"
                   >
                     {item[0].split("\n")[0]}
                   </a>
                 </CardTitle>
-                <CardDescription>{item[1]}</CardDescription>
+
+                <div className="">
+                  <div className="text-sm">
+                    {item[5] !== "—" ? item[5] : 0} ユーザー
+                  </div>
+                  <span className="flex gap-1 pt-1">
+                    <Badge>{item[1]}</Badge>
+                    {item[10]
+                      ? item[10]
+                          .split(",")
+                          .map((tag: string, i: number) => (
+                            <Badge key={i}>{tag.trim()}</Badge>
+                          ))
+                      : null}
+                  </span>
+                </div>
+
+                <CardDescription className="line-clamp-2">
+                  {item[9]}
+                </CardDescription>
               </CardHeader>
             </Card>
           ))}
