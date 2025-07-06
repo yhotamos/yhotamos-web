3d8af84eb496a7769977eb7287b52a2669cedf40
yhotta240 <yhotta240@icloud.com>
2025-07-06 12:08:58 +0900

    fix: インポートを修正

--- src/app/support/page.tsx
+++ src/app/support/page.tsx
@@ -1,10 +1,10 @@
 // "use server";
 
 import Image from "next/image";
-import Header from "../../components/layout/header";
-import Footer from "../../components/layout/footer";
-import { Theme } from "../../components/layout/theme";
-import TopArrowIcon from "../../components/layout/topArrowIcon";
+import Header from "@/components/layout/header";
+import Footer from "@/components/layout/footer";
+import { Theme } from "@/components/layout/theme";
+import TopArrowIcon from "@/components/layout/topArrowIcon";
 
 export default async function Home() {
   const theme = await Theme();
